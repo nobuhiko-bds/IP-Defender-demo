60c92d09776176857b94145a34a3c656ce35670a
Nobuhiko Katsuoka <nobuhiko@blackduck.com>
2025-10-03 13:14:58 +0900

    commit-005

---
+++ Crypt.java
@@ -0,0 +1,18 @@
+package org.owasp.webgoat.lessons.cryptography;
+
+import org.owasp.webgoat.container.lessons.Category;
+import org.owasp.webgoat.container.lessons.Lesson;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cryptography extends Lesson {
+  @Override
+  public Category getDefaultCategory() {
+    return Category.A2;
+  }
+
+  @Override
+  public String getTitle() {
+    return "6.crypto.title"; // first lesson in general
+  }
+}
